c22c0aa8800270037457b3f58f594a86dddedfb5
luka <limarkov@edu.hse.ru>
2026-01-20 21:40:02 +0300

    v1 ocr

---
+++ src/models/ocr_v1.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+
+
+class OCRv1(nn.Module):
+    def __init__(self, num_chars=22, max_length=9):
+        super().__init__()
+        self.max_length = max_length
+        self.num_chars = num_chars
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 32)),  # (B, 128, 1, 32)
+        )
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(128 * 32, max_length * num_chars)
+
+    def forward(self, x):
+        x = self.cnn(x)  # (B, 128, 1, 32)
+        x = self.flatten(x)  # (B, 4096)
+        x = self.fc(x)  # (B, 198)
+        x = x.view(
+            -1, self.max_length, self.num_chars
+        )  # (B, 9, 22) 9 позиций, алфавит из 22 символов.
+        return x
