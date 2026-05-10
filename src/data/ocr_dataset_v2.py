dd6f14b76b0b8b05ba5c25a52f1da57e2cf06723
luka <limarkov@edu.hse.ru>
2026-05-10 17:27:39 +0300

    removed commented code

--- src/data/ocr_dataset_v2.py
+++ src/data/ocr_dataset_v2.py
@@ -3,24 +3,6 @@ from PIL import Image
 import torch
 from torchvision import transforms
 
-# class LetterboxResize:
-#     def __init__(self, size, fill=255):
-#         self.size = size
-#         self.fill = fill
-
-#     def __call__(self, img):
-#         w, h = img.size
-#         target_w, target_h = self.size
-#         scale = min(target_w / w, target_h / h)
-#         new_w, new_h = int(w * scale), int(h * scale)
-#         img = img.resize((new_w, new_h), Image.Resampling.BICUBIC)
-
-#         new_img = Image.new('L', (target_w, target_h), self.fill)
-#         paste_x = (target_w - new_w) // 2
-#         paste_y = (target_h - new_h) // 2
-#         new_img.paste(img, (paste_x, paste_y))
-#         return new_img
-
 
 class OCRDatasetV2(torch.utils.data.Dataset):
     def __init__(
